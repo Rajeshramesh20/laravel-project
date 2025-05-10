0903b579fb7b3173a9706def62319936b7ea93e8
rajesh <r.rajeshramesh2025@gmail.com>
2025-05-10 11:51:30 +0530

    changes in css fils

--- resources/views/layouts/form_footer.blade.php
+++ resources/views/layouts/form_footer.blade.php
@@ -1,16 +1,18 @@
 <footer>
 <div class="footercontainer">
+    <div class="footerwrapper">
 <!-- WhatsApp -->
-<a href="https://wa.me/your_number" target="_blank"><i class="fab fa-whatsapp"></i></a>
+<a href="https://wa.me/your_number" target="_blank"><i class="fab fa-whatsapp icons" ></i></a>
 
 <!-- Facebook -->
-<a href="https://facebook.com/your_profile" target="_blank"><i class="fab fa-facebook"></i></a>
+<a href="https://facebook.com/your_profile" target="_blank"><i class="fab fa-facebook icons"></i></a>
 
 <!-- Instagram -->
-<a href="https://instagram.com/your_profile" target="_blank"><i class="fab fa-instagram"></i></a>
+<a href="https://instagram.com/your_profile" target="_blank"><i class="fab fa-instagram icons"></i></a>
 
 <!-- Email -->
-<a href="mailto:your@email.com"><i class="fas fa-envelope"></i></a>
+<a href="mailto:your@email.com"><i class="fas fa-envelope" icons></i></a>
+</div>
 </div>
 
     
